cb321d95f34da346b687c10b719aa52b4472a40f
konojunya <junya03dance@gmail.com>
2017-09-02 18:11:16 +0900

    init

---
+++ yosenabe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yosenabe
+//
+//  Created by konojunya on 2017/09/02.
+//  Copyright © 2017年 konojunya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
