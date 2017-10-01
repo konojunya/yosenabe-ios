01ef5a99de8552a19b7af98d6d09d848bbd950db
konojunya <junya03dance@gmail.com>
2017-10-01 16:27:59 +0900

    designを先につくる

--- yosenabe-ios/ViewController.swift
+++ yosenabe-ios/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func btn(_ sender: Any) {
+        self.pushView()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func pushView() {
+        let storyboard: UIStoryboard = self.storyboard!
+        let nextView = storyboard.instantiateViewController(withIdentifier: "second")
+        let navi = UINavigationController(rootViewController: nextView)
+        present(navi, animated: true, completion: nil)
     }
 
 
